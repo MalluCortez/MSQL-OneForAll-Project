2e1a745906c1cdb6830f14ae88704d38dbe0fe59
MalluCortez <maria.santana@outlook.com.br>
2023-05-15 18:09:45 -0300

    requisito 3 feito

---
+++ challenges/desafio3.sql
@@ -0,0 +1,12 @@
+SELECT
+	u.user_name AS pessoa_usuaria,
+    COUNT(replay.user_id) AS musicas_ouvidas,
+    ROUND((SUM(s.duration_seconds) / 60), 2) AS total_minutos 
+FROM 
+	reproduction_history As replay
+		INNER JOIN
+	users AS u ON replay.user_id = u.user_id
+		INNER JOIN 
+	songs AS s ON replay.song_id = s.song_id
+    GROUP BY pessoa_usuaria
+    ORDER BY pessoa_usuaria;
